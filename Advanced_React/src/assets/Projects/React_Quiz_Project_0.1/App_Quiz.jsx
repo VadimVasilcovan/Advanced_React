f3f9f016ff64b6c8d1e87244a50b28938fa7c91f
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-28 16:52:45 +0200

    28.03.2025

--- Advanced_React/src/assets/Projects/React_Quiz_Project_0.1/App_Quiz.jsx
+++ Advanced_React/src/assets/Projects/React_Quiz_Project_0.1/App_Quiz.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import "./index.css";
 
 export default function AppReactQuiz() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error('error'));
+  }, []);
   return (
     <div className="app">
       <Header />
