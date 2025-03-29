9dd60001eae62d05df2ce80f25860550f932cddf
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-29 23:04:03 +0200

    29.03.2025

--- Advanced_React/src/assets/exercises/useReducer_Ex/useReducer_EX.03.jsx
+++ Advanced_React/src/assets/exercises/useReducer_Ex/useReducer_EX.03.jsx
@@ -1,40 +1,45 @@
-import { useEffect, useReducer } from "react"
-
+import { useEffect, useReducer } from "react";
 
 const initialState = {
-    question: [],
-    status: 'loading',
-}
+  question: [],
+  status: "loading",
+};
 
-const reducer = (state, action)=>{
-    switch(action.type){
-        case 'dataReceived':
-            return {...state, question: action.payload, status: 'ready'}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "FailedToLoad":
+      return { ...state, status: "error" };
+    default:
+      return state;
+  }
+};
+
+export default function UseReducerEX03() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const data = await fetch("https://jsonplaceholder.typicode.com/users");
+        const response = await data.json();
+        dispatch({ type: "dataReceived", payload: response });
+      } catch (err) {
+        console.err(err);
+        dispatch({ type: "FailedToLoad", payload: err });
+      }
     }
-}
-
-
-
-
-export default function UseReducerEX03(){
-    const [state, dispatch] = useReducer(reducer, initialState)
-    
-
-    useEffect(()=>{
-        async function getData () {
-            try{
-                const data = await fetch('https://jsonplaceholder.typicode.com/users')
-                const response = await data.json();
-                dispatch({type: 'dataReceived', payload: response})
-            }catch(err){
-                console.err(err)
-                dispatch({type: 'FailedToLoad', payload: err })
-            }
-        }
-        getData();
-    }, [])
-    return(
-        <>
-        </>
-    )
+    getData();
+  }, []);
+  return (
+    <>
+      {state.status === "loading" && <p>Loading...</p>}
+      {state.status === "errot" && <p>Error loading data.</p>}
+      {state.status === "ready" &&
+        state.question.map((user) => {
+          <p key={user.id}>{user.name}</p>;
+        })}
+    </>
+  );
 }
